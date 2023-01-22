9706c6cf01bf218820c1d169f7e303c814ca10a8
michealsource <kelvinmicheal209@gmail.com>
2023-01-22 11:52:05 +0100

    update

--- src/components/TradingSection/TradingSection.jsx
+++ src/components/TradingSection/TradingSection.jsx
@@ -6,22 +6,25 @@ import Col from 'react-bootstrap/Col';
 import AppButton from '../AppButton/AppButton';
 const TradingSection = () => {
     return (
-        <Row className={styles.tradingContainer}>
-            <Col class="col-12 col-md-6" data-aos="fade-right"
-                data-aos-easing="ease-out-cubic"
-                data-aos-duration="2000" >
-                <img src={trading} alt="" className={styles.tradingImg} />
-            </Col>
-            <Col class="col-12 col-md-6" data-aos="fade-left"
-                data-aos-easing="ease-out-cubic"
-                data-aos-duration="2000" >
-                <h6 className={styles.tradingH6Text}>BEST TRADING EXPERIENCE</h6>
-                <h5 className={styles.tradingH5Text}>Trade Anywhere & Anytime!</h5>
-                <p className={styles.tradingText}>Welcome to Dyleum, the one-stop protocol for all crypto users! We’re here to make crypto transactions easier and more secure, leveraging the power of blockchain with fintech. Whether you’re a beginner or an experienced investor, we have the tools to help you manage your crypto portfolio. With our intuitive interface, you can easily buy, sell, and store your digital assets. Plus, our advanced security features provide peace of mind that your funds are safe and secure. Get started today and join the future of finance with Dyleum.</p>
-                <AppButton
-                    title="Read more"
-                />
-            </Col>
+        <Row style={{ width: "100%", marginLeft: 'auto', marginRight: 'auto' }}>
+            <div className={styles.tradingContainer}>
+                <Col class="col-12 col-md-6" data-aos="fade-right"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="2000" >
+                    <img src={trading} alt="" className={styles.tradingImg} />
+                </Col>
+                <Col class="col-12 col-md-6" data-aos="fade-left"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="2000" >
+                    <h6 className={styles.tradingH6Text}>BEST TRADING EXPERIENCE</h6>
+                    <h5 className={styles.tradingH5Text}>Trade Anywhere & Anytime!</h5>
+                    <p className={styles.tradingText}>Welcome to Dyleum, the one-stop protocol for all crypto users! We’re here to make crypto transactions easier and more secure, leveraging the power of blockchain with fintech. Whether you’re a beginner or an experienced investor, we have the tools to help you manage your crypto portfolio. With our intuitive interface, you can easily buy, sell, and store your digital assets. Plus, our advanced security features provide peace of mind that your funds are safe and secure. Get started today and join the future of finance with Dyleum.</p>
+                    <AppButton
+                        title="Read more"
+                    />
+                </Col>
+            </div>
+
         </Row>
     )
 }

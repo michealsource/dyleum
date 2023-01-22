447e828b35161690e73abd8d126056ae142eff9c
michealsource <kelvinmicheal209@gmail.com>
2023-01-22 14:14:31 +0100

    new update

--- src/components/RoadMap/RoadMap.jsx
+++ src/components/RoadMap/RoadMap.jsx
@@ -4,7 +4,10 @@ import roadMapMobile from '../../assets/images/roadmapMobile.png'
 import styles from './RoadMap.module.css'
 const RoadMap = () => {
     return (
-        <div className={styles.roadMapContainer}>
+        <div className={styles.roadMapContainer}
+            data-aos="zoom-in-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000">
             <img src={roadMap} alt="" className={styles.roadMapBg} />
             <img src={roadMapMobile} alt="" className={styles.roadMapBgMobileScreen} />
         </div>
